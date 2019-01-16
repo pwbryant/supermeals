36b81a9e85b8b60051a57ffe73e6709f78d91a97
Paul Bryant <paulwilliambryant@gmail.com>
2019-01-16 08:37:20 -0600

    fiddling with db settings for deploy

--- supermeals/settings.py
+++ supermeals/settings.py
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'supermeals.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
+# if 'RDS_DB_NAME' in os.environ:
+if 'DJANGO_SECRET_KEY' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
